bfd52862cd44d98a6e58cfbaddce780ce582771d
victor mongare <67438856+victoroki@users.noreply.github.com>
2025-02-13 12:31:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -77,4 +77,5 @@ def upload_file():
         return jsonify(prediction=predicted_class)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host = "0.0.0.0", port = port)
